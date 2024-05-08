2fb8c8ec2db1e2ce115f17b021e28475f8bf2de3
Tommy <tsegovia20@mail.wou.edu>
2024-05-08 00:37:30 -0700

    Added basic home page

--- Portfolio/src/Home/Home.tsx
+++ Portfolio/src/Home/Home.tsx
@@ -1,7 +1,15 @@
+import styles from './Home.css';
 function Home(){
     return(
-        <div>
-            <h1>wehome</h1>
+        <div id="mainPage">
+            <h1>Welcome to my CS 407 Portfolio</h1>
+            <div id="summaryDiv">
+                <p>
+                    This is my portfolio for CS 407. It contains all the assignments I have completed for the computer graphics class so far. <br/>
+                    My Name is <strong>Tommy Segovia</strong> and I am a Computer Science student at Western Oregon University.<br/>
+                    Check out all of the work <a href="https://github.com/TommySegovia/CS407Portfolio">here</a> on my GitHub.
+                </p>
+            </div>
         </div>
     )
 }
