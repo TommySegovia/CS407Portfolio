ff4f6e50e6c46e8492a3f73518c3fd565d05cdbd
Tommy <tsegovia20@mail.wou.edu>
2024-06-10 04:21:48 -0700

    Fixed navbar and final header text

--- Portfolio/src/Final/Final.tsx
+++ Portfolio/src/Final/Final.tsx
@@ -16,37 +16,24 @@ function Final() {
 
   return (
     <div>
+      <h1 className={styles.h1}>Final</h1>
       <div id="summaryDiv" className={styles.summaryDiv}>
-    <p>
-      The goal of this assignment is to use what we've learned to make something new that's interesting and fun
-    </p>
-  </div>
-  <div id="listDiv" className={styles.listDiv}>
-    <ul>
-    <li>
-        Click on the window to lock the mouse
-      </li>
-      <li>
-        Use WASD to move
-      </li>
-      <li>
-        Use C to crouch
-      </li>
-      <li>
-        Use Space to jump
-      </li>
-      <li>
-        Move the mouse to look around
-      </li>
-      <li>
-        Use M to toggle ambient sounds (Off by default)
-      </li>
-    </ul>
-  </div>
-      <div className={styles.container} ref={containerRef}>
-        <h1 className={styles.h1}>Final</h1>
+        <p>
+          The goal of this assignment is to use what we've learned to make
+          something new that's interesting and fun
+        </p>
       </div>
-      
+      <div id="listDiv" className={styles.listDiv}>
+        <ul>
+          <li>Click on the window to lock the mouse</li>
+          <li>Use WASD to move</li>
+          <li>Use C to crouch</li>
+          <li>Use Space to jump</li>
+          <li>Move the mouse to look around</li>
+          <li>Use M to toggle ambient sounds (Off by default)</li>
+        </ul>
+      </div>
+      <div className={styles.container} ref={containerRef}></div>
     </div>
   );
 }
